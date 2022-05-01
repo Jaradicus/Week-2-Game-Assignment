ae1b1841d1537cb18fa1406f8c06845fb7c0327f
Jared Hogan <35079146+Jaradicus@users.noreply.github.com>
2022-04-30 19:59:29 -0600

    Update Tic-Tac-Toe.py

--- Tic-Tac-Toe.py
+++ Tic-Tac-Toe.py
@@ -1,40 +1,50 @@
-grid = {
-    0 : ["error"],
-    1 : [0],
-    2 : [0],
-    3 : [0],
-    4 : [0],
-    5 : [0],
-    7 : [0],
-    8 : [0],
-    9 : [0]
+grid_bones = {
+    0 : "error",
+    1 : 1,
+    2 : 2,
+    3 : 3,
+    4 : 4,
+    5 : 5,
+    6 : 6,
+    7 : 7,
+    8 : 8,
+    9 : 9
 }
 
 game_active = 1
 active_player = "X"
 selected_tile = 0
 
-#def take_tile(tile):
-    
+def take_tile(tile):
+    global active_player
+    grid_bones[tile] = active_player
 
 def take_turns():
-    while game_active == 1:
-        selected_tile = int(input(f"{active_player}'s turn. Which tile would you like to take?\n"))
-        if selected_tile == 0:
-            take_tile(selected_tile)
-            break
-        else :
-            print("That tile is already taken!")
-            break
+    global active_player, selected_tile
+    while game_active == 1:     
+        while game_active == 1:
+            get_grid()
+            selected_tile = int(input(f"{active_player}'s turn. Which tile would you like to take?\n"))
+            if grid_bones[selected_tile] == selected_tile:
+                take_tile(selected_tile)
+                if active_player == "X":
+                    active_player = "O"
+                else:
+                    active_player ="X"
+            else :
+                print("That tile's taken!")
+
+def get_grid():
+    print(f"{grid_bones[1]}|{grid_bones[2]}|{grid_bones[3]}\n-----\n{grid_bones[4]}|{grid_bones[5]}|{grid_bones[6]}\n-----\n{grid_bones[7]}|{grid_bones[8]}|{grid_bones[9]}\n")
+
 
 def main():
-    global game_active, active_player, selected_tile
-    while game_active == 1:
-        ready = input("Are you ready to start? \nY/N\n")
-        if ready == "Y":
-            take_turns()
-        elif ready == "N":
-            game_active = 0
-        else:
-            print("game start error")
+    global game_active, active_player
+    ready = input("Are you ready to start? \nY/N\n")
+    if ready == "Y":
+        take_turns()
+    elif ready == "N":
+        game_active = 0
+    else:
+        print("game start error")
 main()
